7cd424e6499d2b149569d913100d348148bb744c
Yate9 <lucasmourisabeijon06@liceolapaz.net>
2025-09-30 09:23:18 +0200

    ejerciciodisco

--- src/ejercicio5.java
+++ src/ejercicio5.java
@@ -3,10 +3,42 @@ import java.util.Scanner;
 public class ejercicio5  {
     public static void main(String[] args) {
         System.out.println("Ey pon un precio");
+        double nivelalcohol = 0.0;
+        int preciochupito = 3;
+        double preciocaña = 1.8;
+        double preciocombinado = 8;
+        double nivelchupito = 0.25;
+        double nivelcaña = 0.1;
+        double nivelcombinado = 0.4;
+
+
+        System.out.println("Como te llamas?");
         Scanner sc = new Scanner(System.in);
-        double precio = sc.nextDouble();
-        if (precio >= 100)
-            precio = precio * 0.85;
-      System.out.println(precio);
-    }
-}
+        String nombreusuario = sc.nextLine();
+        System.out.println("Que edad tienes");
+        int edadcliente = sc.nextInt();
+        System.out.println("Cuanto dinero tienes?");
+        double dinerocliente = sc.nextDouble();
+
+
+        System.out.println("El cliente de nombre: " + nombreusuario +"con edad"+ edadcliente +"Tiene" + dinerocliente+"€");
+
+        if (edadcliente<=17) {
+            System.out.println("No puedes entrar eres menor de edad");;
+            System.exit(0);
+        } else {
+            System.out.println("Puedes entrar, que eres mayor de edad");
+            System.out.print("Que nivel de alcohol tienes?");
+            double nivelalalcohol = sc.nextDouble();
+            if (nivelalalcohol>=1.2) {
+                System.out.println("No puedes entrar, vas muy borracho");
+                System.exit(0);
+            } else
+                System.out.println("Puedes pasar");
+            while(dinerocliente >= 1.8) {
+                System.out.print("Que quieres e beber?");
+                System.out.println(dinerocliente);
+            }
+            }
+        }
+   }
---
+++ src/ejercicio7.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ejercicio7 {
+    public static void main(String[] args) {
+        System.out.println("Ey bienvenido a la fiesta");
+        Scanner sc = new Scanner(System.in);
+        double precio = sc.nextDouble();
+
+
+
+    }
+}
